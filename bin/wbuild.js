74c04ddefcef9eeb38c9aad72a958aeed4dd4036
Vidar Eldøy <vidar@eldoy.com>
2019-10-11 21:27:47 +0200

    Update

--- bin/wbuild.js
+++ bin/wbuild.js
@@ -1,20 +1,17 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process')
 const path = require('path')
-
-const fpath = path.resolve(path.join(__dirname, '..'))
-console.log(fpath)
-process.chdir(fpath)
-
 function run () {
   require(fpath + '/lib/builder.js')()
 }
-
 try {
   run()
 } catch (e) {
   if (e.code === 'MODULE_NOT_FOUND') {
+    const fpath = path.resolve(path.join(__dirname, '..'))
+    process.chdir(fpath)
     console.log('Please wait, installing rollup...')
+    console.log(fpath)
     execSync('npm i')
     console.log('Done. Run "wbuild" again to start building.')
   }
